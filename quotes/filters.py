14a21a742ceefcf43d8b18f4459615d9a43c7564
khabibrahman.lera <khabibrahman.lera@gmail.com>
2025-09-09 02:35:49 +0700

    Изменение фильтров

--- quotes/filters.py
+++ quotes/filters.py
@@ -1,8 +1,7 @@
 import django_filters
-from .models import Quote
 
 import django_filters
-from django.db.models import F
+from django import forms
 from .models import Quote
 
 class QuoteFilter(django_filters.FilterSet):
@@ -21,54 +20,62 @@ class QuoteFilter(django_filters.FilterSet):
     likes_order = django_filters.ChoiceFilter(
         field_name='likes',
         choices=(('asc', 'По возрастанию'), ('desc', 'По убыванию')),
-        method='filter_likes_order'
+        method='collect_likes_order'
     )
     
     dislikes_order = django_filters.ChoiceFilter(
         field_name='dislikes',
         choices=(('asc', 'По возрастанию'), ('desc', 'По убыванию')),
-        method='filter_dislikes_order'
+        method='collect_dislikes_order'
     )
     
     views_order = django_filters.ChoiceFilter(
         field_name='views',
         choices=(('asc', 'По возрастанию'), ('desc', 'По убыванию')),
-        method='filter_views_order'
+        method='collect_views_order'
     )
     
-    date_from = django_filters.DateFilter(field_name='created_at', label='Дата добавления (начало)', lookup_expr='gte')
-    date_to = django_filters.DateFilter(field_name='created_at', label='Дата добавления (конец)', lookup_expr='lte')
+    date_from = django_filters.DateFilter(field_name='created_at', label='Дата добавления (начало)', lookup_expr='gte', widget=forms.DateInput(attrs={"type": "date"}))
+    date_to = django_filters.DateFilter(field_name='created_at', label='Дата добавления (конец)', lookup_expr='lte', widget=forms.DateInput(attrs={"type": "date"}))
     
-    limit = django_filters.NumberFilter(label='Число значений', method='filter_limit')
+    limit = django_filters.NumberFilter(label='Число значений', method='collect_limit')
 
     class Meta:
         model = Quote
-        fields = []
+        fields = ["likes_order", "dislikes_order", "views_order", "date_from", "date_to", "limit"]
+    
+    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
+        super().__init__(data=data, queryset=queryset, request=request, prefix=prefix)
+        self._collected_ordering = []
+        self._limit = None 
 
-    def filter_likes_order(self, queryset, name, value):
-        if value == 'asc':
-            return queryset.order_by('likes')
-        elif value == 'desc':
-            return queryset.order_by('-likes')
+    def collect_likes_order(self, queryset, name, value):
+        if value:
+            self._collected_ordering.append("likes" if value == "asc" else "-likes")
+        return queryset
+
+    def collect_dislikes_order(self, queryset, name, value):
+        if value:
+            self._collected_ordering.append("dislikes" if value == "asc" else "-dislikes")
+        return queryset
+
+    def collect_views_order(self, queryset, name, value):
+        if value:
+            self._collected_ordering.append("views" if value == "asc" else "-views")
         return queryset
     
-    def filter_dislikes_order(self, queryset, name, value):
-        if value == 'asc':
-            return queryset.order_by('dislikes')
-        elif value == 'desc':
-            return queryset.order_by('-dislikes')
+    def collect_limit(self, queryset, name, value):
+        if value is not None and value > 0:
+            self._limit = int(value)
         return queryset
 
-    def filter_views_order(self, queryset, name, value):
-        if value == 'asc':
-            return queryset.order_by('views')
-        elif value == 'desc':
-            return queryset.order_by('-views')
-        return queryset
+    def filter_queryset(self, queryset):
+        qs = super().filter_queryset(queryset)
 
-    def filter_limit(self, queryset, name, value):
-        try:
-            limit = int(value)
-            return queryset[:limit]
-        except (ValueError, TypeError):
-            return queryset
+        if self._collected_ordering:
+            qs = qs.order_by(*self._collected_ordering)
+
+        if self._limit:
+            qs = qs[:self._limit]
+
+        return qs
